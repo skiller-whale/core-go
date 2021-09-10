5462a4722547f24b79798a3ac6fecf741213844d
Dave Millican <dave@skillerwhale.com>
2021-09-10 13:27:47 +0100

    Add exercises for loops

---
+++ exercises/loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 5
+
+	// Printf will output on the same line until a line break is added
+	fmt.Printf("Multiples of %v: ", number)
+
+	// Replace with a for loop
+	fmt.Printf("%v ", 1*5)
+	fmt.Printf("%v ", 2*5)
+
+	// Called once to add a line break
+	fmt.Println()
+
+}
---
+++ exercises/loops/range/range.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	names := [3]string{"Alexander Clamilton", "Free Willy Nelson", "Justfin Swimberlake"}
+
+	// TODO
+
+}
