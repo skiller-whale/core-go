487688ddd7f5f5a48d3cc6a5cd9d39fc66f0f302
Neill Pearman <n.pearman@gmail.com>
2021-10-26 09:00:57 +0200

    Publish maps exercise

---
+++ exercises/maps/main.go
@@ -0,0 +1,26 @@
+package main
+
+func findCountry(codeToName map[string]string) {
+	// var input string
+	// fmt.Scan(&input)
+
+	// Uncomment above lines and write code below this
+
+}
+
+func main() {
+
+	// Exercise 1: Create a map of codes to country names
+
+	// Exercise 2: Add new entries to codeToName
+
+	// Exercise 3: Find how many country codes are stored in codeToName
+
+	// Exercise 4: User enters a code, print the respective country else print not found
+	// findCountry(codeToName)
+
+	// Exercise 5: Create a map of beginning letter to slice of codes
+
+	// fmt.Println("Total number of country codes beginning with A:", "---")
+	// fmt.Println("Total number of country codes beginning with Z:", "---")
+}
---
+++ exercises/maps/set/set.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// Declare set here
+
+	// ----------------
+	// fmt.Println(text)
+
+	// for {
+	// 	var word string
+	// 	fmt.Scanln(&word)
+
+	// }
+}
